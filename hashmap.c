189e26815c1b7015e9962d9b8b2d738bbf936b17
Joeb3219 <joseph.a.boyle@rutgers.edu>
2017-09-19 18:01:45 -0400

    Some comments for clarity :)

--- hashmap.c
+++ hashmap.c
@@ -16,16 +16,23 @@ unsigned long long hash(char* key){
         return value;
 }
 
+// Generates a hashmap of HASHMAP_DEFAULT_SIZE.
+// Hashmaps can store anything as their data section as a pointer.
 Hashmap* generateHashmap(){
 	int i;
 	Hashmap* map = malloc(sizeof(Hashmap));
 	map->capacity = HASHMAP_DEFAULT_SIZE;
 	map->data = malloc(sizeof(void*) * HASHMAP_DEFAULT_SIZE);
 	map->filled = 0;
+	// By default, we set every pointer to NULL. This helps when we later empty it,
+	// as we can simply check if the data is NULL.
 	for(i = 0; i < map->capacity; i ++) map->data[i] = NULL;
 	return map;
 }
 
+// Destroys the hashmap.
+// This is equivalent to freeing all of the data pointers and the hashmap itself.
+// WARNING: anything inserted into the hashmap will be FREEd.
 void destroyHashmap(Hashmap* map){
 	int i = 0;
 	for(i = 0; i < map->capacity; i ++){
@@ -35,9 +42,13 @@ void destroyHashmap(Hashmap* map){
 	free(map);
 }
 
+// Inserts some value at a key in the hashmap.
 void hashmapInsert(Hashmap *map, char* key, void* value){
 	int i;
 	map->filled ++;
+	// If inserting this entry yields more entries total than we have space for,
+	// we will double the size of the array (And thus capacity).
+	// To do so, we null-ify all of the new entries in the block of memory we requested.
 	if(map->filled == map->capacity){
 		map->capacity *= 2;
 		map->data = realloc(map->data, sizeof(void*) * map->capacity);
@@ -46,6 +57,9 @@ void hashmapInsert(Hashmap *map, char* key, void* value){
 	map->data[hash(key) % map->capacity] = value;
 }
 
+// Reads the data at some key in the hashmap.
 void* hashmapRead(Hashmap *map, char* key){
+	// We find the hash, and use modulo to wrap it such that it is within the capacity.
+	// IE: if capacity is 5, and hash is 9, our key will be 4.
 	return map->data[hash(key) % map->capacity];
 }
